7e81b142fb3f053201e8603e6dd88bd15a0d07c4
juan diego <jdvc171@hotmail.com>
2019-03-12 17:25:15 +0100

    PantallaPrincipal(Creación lista desplegable "Sin función")

--- app/src/main/java/studium/practicat7/practicat7/MainActivity.java
+++ app/src/main/java/studium/practicat7/practicat7/MainActivity.java
@@ -6,17 +6,26 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    Spinner lista1;
+    ArrayAdapter adapEstados;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        lista1 = findViewById(R.id.listaCat);
+
+        adapEstados =  ArrayAdapter.createFromResource(this,R.array.Categorias,
+                android.R.layout.simple_spinner_dropdown_item);
+
+        lista1.setAdapter(adapEstados);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
